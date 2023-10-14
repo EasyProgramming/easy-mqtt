75137040516a5b2f32079b5b166c24fb82cc8eb9
zbz <913734187@qq.com>
2023-10-15 01:44:44 +0800

    refactor:代码格式化及针对集群scan的处理

--- server/src/main/java/com/ep/mqtt/server/util/RedisTemplateUtil.java
+++ server/src/main/java/com/ep/mqtt/server/util/RedisTemplateUtil.java
@@ -1,21 +1,5 @@
 package com.ep.mqtt.server.util;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
-import io.lettuce.core.*;
-import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.api.async.RedisAsyncCommands;
-import io.lettuce.core.api.sync.RedisHashCommands;
-import io.lettuce.core.api.sync.RedisSetCommands;
-import io.lettuce.core.cluster.api.async.RedisClusterAsyncCommands;
-import io.lettuce.core.cluster.api.sync.RedisClusterCommands;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.util.CollectionUtils;
-
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
@@ -25,6 +9,19 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
 
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.CollectionUtils;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import io.lettuce.core.*;
+import io.lettuce.core.cluster.api.async.RedisClusterAsyncCommands;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author zbz
  * @date 2023/8/15 18:06
@@ -32,39 +29,43 @@ import java.util.function.Consumer;
 @Slf4j
 public class RedisTemplateUtil {
 
-    public static RedisClusterAsyncCommands<byte[], byte[]> getStatefulRedisConnection(RedisConnection connection){
+    public static RedisClusterAsyncCommands<byte[], byte[]> getStatefulRedisConnection(RedisConnection connection) {
         if (connection.isQueueing() || connection.isPipelined()) {
             throw new UnsupportedOperationException("'SCAN' cannot be called in pipeline / transaction mode.");
         }
         Object nativeConnection = connection.getNativeConnection();
-        if (nativeConnection instanceof RedisClusterAsyncCommands){
+        if (nativeConnection instanceof RedisClusterAsyncCommands) {
             return (RedisClusterAsyncCommands<byte[], byte[]>)nativeConnection;
         }
         throw new UnsupportedOperationException("only support lettuce commands");
 
     }
 
-    public static Set<String> lettuceScan(RedisConnection connection, String pattern, Integer size, Consumer<String> consumer) {
+    public static Set<String> lettuceScan(RedisConnection connection, String pattern, Integer size,
+        Consumer<String> consumer) {
         RedisClusterAsyncCommands<byte[], byte[]> redisClusterAsyncCommands = getStatefulRedisConnection(connection);
-        ScanCursor cursor = ScanCursor.INITIAL;
-        KeyScanCursor<byte[]> keyScanCursor;
+        KeyScanCursor<byte[]> keyScanCursor = null;
         Set<String> allMatchKeySet = Sets.newHashSet();
         do {
             ScanArgs scanArgs = ScanArgs.Builder.limit(size).match(pattern);
-            RedisFuture<KeyScanCursor<byte[]>> keyScanCursorRedisFuture = redisClusterAsyncCommands.scan(cursor, scanArgs);
+            RedisFuture<KeyScanCursor<byte[]>> keyScanCursorRedisFuture;
+            if (keyScanCursor == null) {
+                keyScanCursorRedisFuture = redisClusterAsyncCommands.scan(scanArgs);
+            } else {
+                keyScanCursorRedisFuture = redisClusterAsyncCommands.scan(keyScanCursor, scanArgs);
+            }
             try {
-               keyScanCursor = keyScanCursorRedisFuture.get(10, TimeUnit.SECONDS);
+                keyScanCursor = keyScanCursorRedisFuture.get(10, TimeUnit.SECONDS);
             } catch (InterruptedException | ExecutionException | TimeoutException e) {
                 log.error("lettuce scan error", e);
                 break;
             }
-            cursor = ScanCursor.of(keyScanCursor.getCursor());
             List<String> matchKeyList = convert(keyScanCursor.getKeys());
-            if (CollectionUtils.isEmpty(matchKeyList)){
-               break;
+            if (CollectionUtils.isEmpty(matchKeyList)) {
+                continue;
             }
-            if (consumer != null){
-                for (String matchKey : matchKeyList){
+            if (consumer != null) {
+                for (String matchKey : matchKeyList) {
                     consumer.accept(matchKey);
                 }
             }
@@ -73,32 +74,36 @@ public class RedisTemplateUtil {
         return allMatchKeySet;
     }
 
-    public static Map<String, String> lettuceHashScan(RedisConnection connection, String hashKey, String pattern, Integer size,
-                                                      Consumer<Map.Entry<String, String>> consumer) {
+    public static Map<String, String> lettuceHashScan(RedisConnection connection, String hashKey, String pattern,
+        Integer size, Consumer<Map.Entry<String, String>> consumer) {
         RedisClusterAsyncCommands<byte[], byte[]> redisClusterAsyncCommands = getStatefulRedisConnection(connection);
-        ScanCursor cursor = ScanCursor.INITIAL;
-        MapScanCursor<byte[], byte[]> mapScanCursor;
+        MapScanCursor<byte[], byte[]> mapScanCursor = null;
         Map<String, String> allMatchMap = Maps.newHashMap();
         do {
             ScanArgs scanArgs = ScanArgs.Builder.limit(size).match(pattern);
-            RedisFuture<MapScanCursor<byte[], byte[]>> mapScanCursorRedisFuture = redisClusterAsyncCommands.hscan(hashKey.getBytes(), cursor, scanArgs);
+            RedisFuture<MapScanCursor<byte[], byte[]>> mapScanCursorRedisFuture;
+            if (mapScanCursor == null) {
+                mapScanCursorRedisFuture = redisClusterAsyncCommands.hscan(hashKey.getBytes(), scanArgs);
+            } else {
+                mapScanCursorRedisFuture = redisClusterAsyncCommands.hscan(hashKey.getBytes(), mapScanCursor, scanArgs);
+            }
             try {
                 mapScanCursor = mapScanCursorRedisFuture.get(10, TimeUnit.SECONDS);
             } catch (InterruptedException | ExecutionException | TimeoutException e) {
                 log.error("lettuce hash scan error", e);
                 break;
             }
-            cursor = ScanCursor.of(mapScanCursor.getCursor());
             Map<byte[], byte[]> matchBytesMap = mapScanCursor.getMap();
-            if (CollectionUtils.isEmpty(matchBytesMap)){
-                break;
+            if (CollectionUtils.isEmpty(matchBytesMap)) {
+                continue;
             }
             Map<String, String> matchMap = Maps.newHashMap();
-            for (Map.Entry<byte[], byte[]> entry : matchBytesMap.entrySet()){
-                matchMap.put(new String(entry.getKey(), StandardCharsets.UTF_8), new String(entry.getValue(), StandardCharsets.UTF_8));
+            for (Map.Entry<byte[], byte[]> entry : matchBytesMap.entrySet()) {
+                matchMap.put(new String(entry.getKey(), StandardCharsets.UTF_8),
+                    new String(entry.getValue(), StandardCharsets.UTF_8));
             }
-            if (consumer != null){
-                for (Map.Entry<String, String> entry : matchMap.entrySet()){
+            if (consumer != null) {
+                for (Map.Entry<String, String> entry : matchMap.entrySet()) {
                     consumer.accept(entry);
                 }
             }
@@ -107,28 +112,32 @@ public class RedisTemplateUtil {
         return allMatchMap;
     }
 
-
-    public static Set<String> lettuceSetScan(RedisConnection connection, String pattern, String setKey, Integer size,Consumer<String> consumer) {
+    public static Set<String> lettuceSetScan(RedisConnection connection, String pattern, String setKey, Integer size,
+        Consumer<String> consumer) {
         RedisClusterAsyncCommands<byte[], byte[]> redisClusterAsyncCommands = getStatefulRedisConnection(connection);
-        ScanCursor cursor = ScanCursor.INITIAL;
-        ValueScanCursor<byte[]> valueScanCursor;
+        ValueScanCursor<byte[]> valueScanCursor = null;
         Set<String> allMatchValueSet = Sets.newHashSet();
         do {
             ScanArgs scanArgs = ScanArgs.Builder.limit(size).match(pattern);
-            RedisFuture<ValueScanCursor<byte[]>> valueScanCursorRedisFuture = redisClusterAsyncCommands.sscan(setKey.getBytes(), cursor, scanArgs);
+            RedisFuture<ValueScanCursor<byte[]>> valueScanCursorRedisFuture;
+            if (valueScanCursor == null) {
+                valueScanCursorRedisFuture = redisClusterAsyncCommands.sscan(setKey.getBytes(), scanArgs);
+            } else {
+                valueScanCursorRedisFuture =
+                    redisClusterAsyncCommands.sscan(setKey.getBytes(), valueScanCursor, scanArgs);
+            }
             try {
                 valueScanCursor = valueScanCursorRedisFuture.get(10, TimeUnit.SECONDS);
             } catch (InterruptedException | ExecutionException | TimeoutException e) {
                 log.error("lettuce set scan error", e);
                 break;
             }
-            cursor = ScanCursor.of(valueScanCursor.getCursor());
             List<String> matchValueList = convert(valueScanCursor.getValues());
-            if (CollectionUtils.isEmpty(matchValueList)){
-                break;
+            if (CollectionUtils.isEmpty(matchValueList)) {
+                continue;
             }
-            if (consumer != null){
-                for (String matchKey : matchValueList){
+            if (consumer != null) {
+                for (String matchKey : matchValueList) {
                     consumer.accept(matchKey);
                 }
             }
@@ -137,19 +146,20 @@ public class RedisTemplateUtil {
         return allMatchValueSet;
     }
 
-    private static List<String> convert(List<byte[]> bytesList){
+    private static List<String> convert(List<byte[]> bytesList) {
         List<String> stringList = Lists.newArrayList();
-        if (CollectionUtils.isEmpty(bytesList)){
+        if (CollectionUtils.isEmpty(bytesList)) {
             return stringList;
         }
-        for (byte[] bytes : bytesList){
+        for (byte[] bytes : bytesList) {
             stringList.add(new String(bytes, StandardCharsets.UTF_8));
         }
         return stringList;
     }
 
     public static <V> Set<String> scan(RedisTemplate<String, V> redisTemplate, String pattern, Integer size) {
-        return redisTemplate.execute((RedisCallback<Set<String>>)connection -> lettuceScan(connection, pattern, size, null));
+        return redisTemplate
+            .execute((RedisCallback<Set<String>>)connection -> lettuceScan(connection, pattern, size, null));
     }
 
     public static <V> void scan(RedisTemplate<String, V> redisTemplate, String pattern, Integer size,
