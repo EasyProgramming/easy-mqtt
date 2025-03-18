3ad819fccd141acde1b37ee5b643b24d3192af75
zbz <zbz>
2025-03-18 13:37:42 +0800

    feature:增加mysql数据库的配置项

--- server/src/main/java/com/ep/mqtt/server/db/component/MySqlDb.java
+++ server/src/main/java/com/ep/mqtt/server/db/component/MySqlDb.java
@@ -17,9 +17,37 @@ import java.util.Map;
 @Component
 public class MySqlDb extends AbstractDb {
 
+    public static String DB_TRANSACTION_ISOLATION = "READ-COMMITTED";
+
+    public static String DB_CHARACTER_SET = "utf8mb4";
+
+    public static String DB_COLLATION = "utf8mb4_general_ci";
+
     @Override
     public void configDb() {
-        // TODO: 2024/12/31 根据元数据，检查数据库的隔离级别是否为rc
+        SqlRunner sqlRunner = getSqlRunner();
+
+        try {
+            List<Map<String, Object>> resultList = sqlRunner.selectAll("SELECT @@transaction_isolation");
+            String transactionIsolation = resultList.get(0).get("@@transaction_isolation").toString();
+            if (!DB_TRANSACTION_ISOLATION.equals(transactionIsolation)){
+                throw new RuntimeException(String.format("MySql的隔离级别应为[%s]", DB_TRANSACTION_ISOLATION));
+            }
+
+            resultList = sqlRunner.selectAll("SHOW VARIABLES LIKE 'character_set_database'");
+            String characterSet = resultList.get(0).get("Value").toString();
+            if (!DB_CHARACTER_SET.equals(characterSet)){
+                throw new RuntimeException(String.format("MySql的字符集应为[%s]", DB_CHARACTER_SET));
+            }
+
+            sqlRunner.selectAll("SHOW VARIABLES LIKE 'collation_database'");
+            String collation = resultList.get(0).get("Value").toString();
+            if (!DB_COLLATION.equals(collation)){
+                throw new RuntimeException(String.format("MySql的排序规则应为[%s]", DB_COLLATION));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
