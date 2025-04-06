ce25e6556b06da0b7ce2e64912f04552c4baabd1
zbz <913734187@qq.com>
2025-04-06 23:02:25 +0800

    feature:删除无用代码

--- server/src/main/java/com/ep/mqtt/server/rpc/EasyMqttRpcClient.java
+++ server/src/main/java/com/ep/mqtt/server/rpc/EasyMqttRpcClient.java
@@ -4,8 +4,6 @@ import com.ep.mqtt.server.metadata.RpcCommand;
 import com.ep.mqtt.server.util.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.CountDownLatch;
-
 /**
  * @author zbz
  * @date 2025/3/25 15:29
@@ -17,32 +15,4 @@ public class EasyMqttRpcClient {
         RpcServer.getRpcVerticle().getEventBus().publish(rpcCommand.getCode(), JsonUtil.obj2String(data));
     }
 
-    public static void distributedLock(String key, Runnable runnable){
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-
-        RpcServer.getVertx().sharedData().getLock(key, (result)->{
-            try {
-                if (!result.succeeded()) {
-                    log.error("获取锁失败, key: [{}]", key, result.cause());
-
-                    return;
-                }
-
-                runnable.run();
-            }
-            catch (Throwable e){
-                log.error("处理失败, key: [{}]", key, e);
-            }
-            finally {
-                result.result().release();
-                countDownLatch.countDown();
-            }
-        });
-
-        try {
-            countDownLatch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
